075e1b069cf985c89d9e95e16728b62aafb318ae
David Caseria <david@Davids-MacBook-Pro.local>
2015-10-20 14:50:06 -0500

    Better vimrc and working on p function

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,8 @@
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 find * -type f ! -name "*.*" -print | while read file; do
+  if [ $(dirname $file) != "." ]; then
+    mkdir -p "$HOME/.$(dirname $file)"
+  fi 
   ln -fs "$(pwd)/$file" "$HOME/.$file"
 done
